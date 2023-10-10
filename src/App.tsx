6c75be7274cbe8a99a08c2fa4975c2bc79fd5b6b
Thhomas1 <thomasromero10@hotmail.com>
2023-10-10 12:03:59 -0300

    react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,64 @@
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home";
+import Products from "./pages/products/Products";
+import Users from "./pages/users/Users";
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+
+
 function App() {
 
-  return (
-    <div>Hello World</div>
-  )
+
+  const Layout = ()=> {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="menuContainer"></div>
+        <Menu />
+        <div className="contentContainer">
+      <Outlet/>
+        </div>
+      <Footer />
+      </div>
+    )
+  }
+
+
+  const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>, // pasamos directamente el layout obvio
+    children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "/users",
+          element: <Users />
+        },
+        {
+          path: "/products",
+          element: <Products />
+        }
+      ]
+    },
+  ]);
+
+  return <RouterProvider router={router} /> ;
+  
 }
-// 4:54
-// yarn para instalar
-// yarn run dev
 
 export default App
+
+
+// 13:51
+// yarn para instalar
+// yarn run dev
