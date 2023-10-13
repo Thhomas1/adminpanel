044b334959cd77199f24fbc40165bbf1d5cf2404
Thhomas1 <thomasromero10@hotmail.com>
2023-10-13 20:19:08 -0300

    menu && data

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,35 +1,28 @@
 import { Link } from "react-router-dom";
-import "./menu.scss"
+import "./menu.scss";
+import {menu} from "../../data"
 
 
 const Menu = () => {
   return (
     <div className="menu">
-      <div className="item">
-        <span className="title">Main</span>
-        <Link to="/">
-        <img src="/home.svg" alt="" />
-        <span className="listItemTitle">Home</span>
-        </Link>
+      {menu.map(item=>(
 
-        <Link to="/">
-        <img src="/profile.svg" alt="" />
-        <span className="listItemTitle">Profile</span>
-        </Link>
+        <div className="item" key={item.id}>
+        <span className="title">{item.title}</span>
+
+       {item.listItems.map((listItem) => (
+         <Link to="/" className="listItem" key={listItem.id}>
+         <img src={listItem.icon} alt="" />
+         <span className="listItemTitle">{listItem.title}</span>
+         </Link>
+
+         ))}
+
       </div>
+
+      ))}
       
-      <div className="item">
-        <span className="title">Main</span>
-        <Link to="/">
-        <img src="/home.svg" alt="" />
-        <span className="listItemTitle">Home</span>
-        </Link>
-
-        <Link to="/">
-        <img src="/profile.svg" alt="" />
-        <span className="listItemTitle">Profile</span>
-        </Link>
-      </div>
     </div>
   )
 }
